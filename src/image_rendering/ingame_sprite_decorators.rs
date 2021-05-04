3e3eb85968ee94c6e1b587dca8bfd10f6ea3ccfd
madwareru <madware.ru@gmail.com>
2021-05-04 22:27:09 +0300

    First attempt to implement .256 rendering

---
+++ src/image_rendering/ingame_sprite_decorators.rs
@@ -0,0 +1,177 @@
+use rom_loaders_rs::images::ingame_sprite::{ImageData, ImageType};
+use crate::image_rendering::blittable::{Blittable, Rect};
+
+pub struct PalettedSpriteRenderingScope<'a> {
+    pub palette: &'a [u32],
+    pub image_data: &'a ImageData,
+    pub img_id: usize
+}
+
+pub struct SpriteRenderingScope<'a> {
+    pub image_data: &'a ImageData,
+    pub img_id: usize
+}
+
+impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
+    fn blit_impl(&self, buffer: &mut [u32], buffer_width: usize, self_rect: Rect, dst_rect: Rect) {
+        match self.image_data.image_type {
+            ImageType::Dot256 => {
+                const BLANK_LINE: u8 = 0x40;
+                const EMPTY_AREA_BITS: u8 = 0xC0;
+                const CHUNK_SIZE_BITS: u8 = 0x3F;
+
+                let current_frame = &self.image_data.frames[self.img_id];
+                let start = current_frame.data_range.start;
+                let end = current_frame.data_range.end;
+                let slice = &self.image_data.raw[start..end];
+
+                let sw = current_frame.width as i32;
+                let (mut dx, mut dy) = (dst_rect.x_range.start as i32, dst_rect.y_range.start as i32);
+                let (mut sx, mut sy) = (0, 0);
+
+                let mut i = 0;
+                while i < slice.len() {
+                    let ipx = slice[i];
+                    let is_empty_area_mask = ipx & EMPTY_AREA_BITS;
+                    let chunk_size = (ipx & CHUNK_SIZE_BITS) as i32;
+                    i += 1;
+
+                    if is_empty_area_mask > 0 {
+                        if is_empty_area_mask == BLANK_LINE {
+                            dy += chunk_size;
+                            sy += chunk_size;
+                        } else {
+                            sx += chunk_size;
+                            dx += chunk_size;
+                            if sx > sw {
+                                dy += 1;
+                                sy += 1;
+                                dx -= sw;
+                                sx -= sw;
+                            }
+                        }
+                        continue;
+                    }
+                    for _ in 0..chunk_size {
+                        let palette_id = slice[i]; i += 1;
+                        if sx >= sw {
+                            dy += 1;
+                            sy += 1;
+                            dx -= sw;
+                            sx -= sw;
+                        }
+                        if sx >= self_rect.x_range.start as i32 &&
+                            sx < self_rect.x_range.end as i32 &&
+                            sy >= self_rect.y_range.start as i32 &&
+                            sy < self_rect.y_range.end as i32 &&
+                            dx >= dst_rect.x_range.start as i32 &&
+                            dx < dst_rect.x_range.end as i32 &&
+                            dy >= dst_rect.y_range.start as i32 &&
+                            dy < dst_rect.y_range.end as i32{
+                            let offset = (dx + dy * buffer_width as i32) as usize;
+                            buffer[offset] = self.palette[palette_id as usize];
+                        }
+                        dx += 1;
+                        sx += 1;
+                    }
+                }
+            }
+            ImageType::Dot16 => {
+
+            }
+            ImageType::Dot16a => {
+
+            }
+        }
+    }
+
+    fn get_width(&self) -> usize {
+        self.image_data.frames[self.img_id].width as usize
+    }
+
+    fn get_height(&self) -> usize {
+        self.image_data.frames[self.img_id].height as usize
+    }
+}
+
+impl<'a> Blittable<u32> for SpriteRenderingScope<'a> {
+    fn blit_impl(&self, buffer: &mut [u32], buffer_width: usize, self_rect: Rect, dst_rect: Rect) {
+        match self.image_data.image_type {
+            ImageType::Dot256 => {
+                const BLANK_LINE: u8 = 0x40;
+                const EMPTY_AREA_BITS: u8 = 0xC0;
+                const CHUNK_SIZE_BITS: u8 = 0x3F;
+
+                let current_frame = &self.image_data.frames[self.img_id];
+                let start = current_frame.data_range.start;
+                let end = current_frame.data_range.end;
+                let slice = &self.image_data.raw[start..end];
+
+                let sw = current_frame.width as i32;
+                let (mut dx, mut dy) = (dst_rect.x_range.start as i32, dst_rect.y_range.start as i32);
+                let (mut sx, mut sy) = (0, 0);
+
+                let mut i = 0;
+                while i < slice.len() {
+                    let ipx = slice[i];
+                    let is_empty_area_mask = ipx & EMPTY_AREA_BITS;
+                    let chunk_size = (ipx & CHUNK_SIZE_BITS) as i32;
+                    i += 1;
+
+                    if is_empty_area_mask > 0 {
+                        if is_empty_area_mask == BLANK_LINE {
+                            dy += chunk_size;
+                            sy += chunk_size;
+                        } else {
+                            sx += chunk_size;
+                            dx += chunk_size;
+                            if sx > sw {
+                                dy += 1;
+                                sy += 1;
+                                dx -= sw;
+                                sx -= sw;
+                            }
+                        }
+                        continue;
+                    }
+                    for _ in 0..chunk_size {
+                        let palette_id = slice[i]; i += 1;
+                        if sx >= sw {
+                            dy += 1;
+                            sy += 1;
+                            dx -= sw;
+                            sx -= sw;
+                        }
+                        if sx >= self_rect.x_range.start as i32 &&
+                            sx < self_rect.x_range.end as i32 &&
+                            sy >= self_rect.y_range.start as i32 &&
+                            sy < self_rect.y_range.end as i32 &&
+                            dx >= dst_rect.x_range.start as i32 &&
+                            dx < dst_rect.x_range.end as i32 &&
+                            dy >= dst_rect.y_range.start as i32 &&
+                            dy < dst_rect.y_range.end as i32{
+                            let offset = (dx + dy * buffer_width as i32) as usize;
+                            buffer[offset] = palette_id as u32;
+                        }
+                        dx += 1;
+                        sx += 1;
+                    }
+                }
+            }
+            ImageType::Dot16 => {
+
+            }
+            ImageType::Dot16a => {
+
+            }
+        }
+    }
+
+    fn get_width(&self) -> usize {
+        self.image_data.frames[self.img_id].width as usize
+    }
+
+    fn get_height(&self) -> usize {
+        self.image_data.frames[self.img_id].height as usize
+    }
+}
--- src/image_rendering/mod.rs
+++ src/image_rendering/mod.rs
@@ -3,3 +3,4 @@ pub mod bresenham;
 pub mod triangles;
 
 pub mod bmp_sprite_decorators;
+pub mod ingame_sprite_decorators;
