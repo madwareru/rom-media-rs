89af8e3fc80c06106e0581020962550b9ab06047
madwareru <madware.ru@gmail.com>
2021-05-08 21:05:27 +0300

    Huge cleanup

--- src/image_rendering/mod.rs
+++ src/image_rendering/mod.rs
@@ -1,6 +1,5 @@
 pub mod blittable;
 pub mod bresenham;
-pub mod triangles;
 
 pub mod bmp_sprite_decorators;
 pub mod ingame_sprite_decorators;
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,3 @@
 pub mod audio;
 pub mod video;
-pub mod windowing;
-pub mod pixel_surface;
 pub mod image_rendering;
-
-#[cfg(not(feature = "use-wgpu"))]
-mod opengl_wrappings;
-
-#[cfg(not(feature = "use-wgpu"))]
-pub mod gl { include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs")); }
-
-#[cfg(not(feature = "use-wgpu"))]
-pub mod graphics {
-    pub type PixelSurface = super::pixel_surface::PixelSurfaceHolder<super::opengl_wrappings::OpenGlSurfaceImpl>;
-    pub use super::opengl_wrappings::clear_background;
-    pub use super::opengl_wrappings::init;
-}
-
-#[cfg(feature = "use-wgpu")]
-mod wgpu_wrappings;
-
-#[cfg(feature = "use-wgpu")]
-pub mod graphics {
-    pub type PixelSurface = super::pixel_surface::PixelSurfaceHolder<super::wgpu_wrappings::WgpuSurfaceImpl>;
-    pub use super::wgpu_wrappings::clear_background;
-    pub use super::wgpu_wrappings::init;
-}
