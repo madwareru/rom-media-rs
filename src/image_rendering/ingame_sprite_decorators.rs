1cd74d7fbec7a24adf364bc66045fc4c0630cba8
madwareru <madware.ru@gmail.com>
2021-05-09 09:12:22 +0300

    Rendering for .16a sprites as well as a bump of deps version

--- src/image_rendering/ingame_sprite_decorators.rs
+++ src/image_rendering/ingame_sprite_decorators.rs
@@ -14,13 +14,12 @@ pub struct SpriteRenderingScope<'a> {
 
 impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
     fn blit_impl(&self, buffer: &mut [u32], buffer_width: usize, self_rect: Rect, dst_rect: Rect) {
+        const BLANK_LINE: u8 = 0x40;
+        const EMPTY_AREA_BITS: u8 = 0xC0;
+        const CHUNK_SIZE_BITS: u8 = 0x3F;
         match self.image_data.image_type {
             ImageType::Dot256 => {
-                const BLANK_LINE: u8 = 0x40;
-                const EMPTY_AREA_BITS: u8 = 0xC0;
-                const CHUNK_SIZE_BITS: u8 = 0x3F;
-
-                let current_frame = &self.image_data.frames[self.img_id];
+                let current_frame = &self.image_data.frames[self.img_id % self.image_data.frames.len()];
                 let start = current_frame.data_range.start;
                 let end = current_frame.data_range.end;
                 let slice = &self.image_data.raw[start..end];
@@ -53,7 +52,7 @@ impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
                         continue;
                     }
                     for _ in 0..chunk_size {
-                        let palette_id = slice[i]; i += 1;
+                        let palette_id = slice[i] as usize; i += 1;
                         if sx >= sw {
                             dy += 1;
                             sy += 1;
@@ -69,7 +68,7 @@ impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
                             dy >= dst_rect.y_range.start as i32 &&
                             dy < dst_rect.y_range.end as i32{
                             let offset = (dx + dy * buffer_width as i32) as usize;
-                            buffer[offset] = self.palette[palette_id as usize];
+                            buffer[offset] = self.palette[palette_id];
                         }
                         dx += 1;
                         sx += 1;
@@ -80,7 +79,65 @@ impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
 
             }
             ImageType::Dot16a => {
+                let current_frame = &self.image_data.frames[self.img_id % self.image_data.frames.len()];
+                let start = current_frame.data_range.start;
+                let end = current_frame.data_range.end;
+                let slice = &self.image_data.raw[start..end];
 
+                let sw = current_frame.width as i32;
+                let (mut dx, mut dy) = (dst_rect.x_range.start as i32, dst_rect.y_range.start as i32);
+                let (mut sx, mut sy) = (0, 0);
+
+                let mut i = 0;
+                while i < slice.len() {
+                    let chunk_size = (slice[i] & CHUNK_SIZE_BITS) as i32; i += 1;
+                    let is_empty_area_mask = slice[i] & EMPTY_AREA_BITS; i += 1;
+                    if is_empty_area_mask > 0 {
+                        if is_empty_area_mask == BLANK_LINE {
+                            dy += chunk_size;
+                            sy += chunk_size;
+                        } else {
+                            sx += chunk_size;
+                            dx += chunk_size;
+                            if sx > sw {
+                                dy += 1;
+                                sy += 1;
+                                dx -= sw;
+                                sx -= sw;
+                            }
+                        }
+                        continue;
+                    }
+                    for _ in 0..chunk_size {
+                        let psh = slice[i] as u16 | ((slice[i+1] as u16) * 0x100); i += 2;
+
+                        // we first shift everything one bit right to get palette identifier
+                        // then we extract alpha. Alpha is four-bit, therefore it takes values
+                        // in a range [0x0..0xF]
+                        let palette_id = ((psh / 0x002) & 0xFF) as usize;
+                        let alpha = ((psh / 0x200) & 0xF) as u32 * 0x11_00_00_00;
+
+                        if sx >= sw {
+                            dy += 1;
+                            sy += 1;
+                            dx -= sw;
+                            sx -= sw;
+                        }
+                        if sx >= self_rect.x_range.start as i32 &&
+                            sx < self_rect.x_range.end as i32 &&
+                            sy >= self_rect.y_range.start as i32 &&
+                            sy < self_rect.y_range.end as i32 &&
+                            dx >= dst_rect.x_range.start as i32 &&
+                            dx < dst_rect.x_range.end as i32 &&
+                            dy >= dst_rect.y_range.start as i32 &&
+                            dy < dst_rect.y_range.end as i32{
+                            let offset = (dx + dy * buffer_width as i32) as usize;
+                            buffer[offset] = alpha | (self.palette[palette_id] & 0x00_FF_FF_FF);
+                        }
+                        dx += 1;
+                        sx += 1;
+                    }
+                }
             }
         }
     }
@@ -97,12 +154,11 @@ impl<'a> Blittable<u32> for PalettedSpriteRenderingScope<'a> {
 /// Implementation for the paletted usage
 impl<'a> Blittable<u16> for SpriteRenderingScope<'a> {
     fn blit_impl(&self, buffer: &mut [u16], buffer_width: usize, self_rect: Rect, dst_rect: Rect) {
+        const BLANK_LINE: u8 = 0x40;
+        const EMPTY_AREA_BITS: u8 = 0xC0;
+        const CHUNK_SIZE_BITS: u8 = 0x3F;
         match self.image_data.image_type {
             ImageType::Dot256 => {
-                const BLANK_LINE: u8 = 0x40;
-                const EMPTY_AREA_BITS: u8 = 0xC0;
-                const CHUNK_SIZE_BITS: u8 = 0x3F;
-
                 let current_frame = &self.image_data.frames[self.img_id];
                 let start = current_frame.data_range.start;
                 let end = current_frame.data_range.end;
@@ -136,7 +192,7 @@ impl<'a> Blittable<u16> for SpriteRenderingScope<'a> {
                         continue;
                     }
                     for _ in 0..chunk_size {
-                        let palette_id = slice[i]; i += 1;
+                        let palette_id = slice[i] as u16; i += 1;
                         if sx >= sw {
                             dy += 1;
                             sy += 1;
@@ -152,7 +208,7 @@ impl<'a> Blittable<u16> for SpriteRenderingScope<'a> {
                             dy >= dst_rect.y_range.start as i32 &&
                             dy < dst_rect.y_range.end as i32{
                             let offset = (dx + dy * buffer_width as i32) as usize;
-                            buffer[offset] = (palette_id as u16) | 0xFF00;
+                            buffer[offset] = palette_id | 0xFF00;
                         }
                         dx += 1;
                         sx += 1;
@@ -163,7 +219,65 @@ impl<'a> Blittable<u16> for SpriteRenderingScope<'a> {
 
             }
             ImageType::Dot16a => {
+                let current_frame = &self.image_data.frames[self.img_id % self.image_data.frames.len()];
+                let start = current_frame.data_range.start;
+                let end = current_frame.data_range.end;
+                let slice = &self.image_data.raw[start..end];
 
+                let sw = current_frame.width as i32;
+                let (mut dx, mut dy) = (dst_rect.x_range.start as i32, dst_rect.y_range.start as i32);
+                let (mut sx, mut sy) = (0, 0);
+
+                let mut i = 0;
+                while i < slice.len() {
+                    let chunk_size = (slice[i] & CHUNK_SIZE_BITS) as i32; i += 1;
+                    let is_empty_area_mask = slice[i] & EMPTY_AREA_BITS; i += 1;
+                    if is_empty_area_mask > 0 {
+                        if is_empty_area_mask == BLANK_LINE {
+                            dy += chunk_size;
+                            sy += chunk_size;
+                        } else {
+                            sx += chunk_size;
+                            dx += chunk_size;
+                            if sx > sw {
+                                dy += 1;
+                                sy += 1;
+                                dx -= sw;
+                                sx -= sw;
+                            }
+                        }
+                        continue;
+                    }
+                    for _ in 0..chunk_size {
+                        let psh = slice[i] as u16 | ((slice[i+1] as u16) * 0x100); i += 2;
+
+                        // we first shift everything one bit right to get palette identifier
+                        // then we extract alpha. Alpha is four-bit, therefore it takes values
+                        // in a range [0x0..0xF]
+                        let palette_id = (psh / 0x002) & 0xFF;
+                        let alpha = ((psh / 0x200) & 0xF) * 0x11_00;
+
+                        if sx >= sw {
+                            dy += 1;
+                            sy += 1;
+                            dx -= sw;
+                            sx -= sw;
+                        }
+                        if sx >= self_rect.x_range.start as i32 &&
+                            sx < self_rect.x_range.end as i32 &&
+                            sy >= self_rect.y_range.start as i32 &&
+                            sy < self_rect.y_range.end as i32 &&
+                            dx >= dst_rect.x_range.start as i32 &&
+                            dx < dst_rect.x_range.end as i32 &&
+                            dy >= dst_rect.y_range.start as i32 &&
+                            dy < dst_rect.y_range.end as i32{
+                            let offset = (dx + dy * buffer_width as i32) as usize;
+                            buffer[offset] = alpha | palette_id;
+                        }
+                        dx += 1;
+                        sx += 1;
+                    }
+                }
             }
         }
     }
